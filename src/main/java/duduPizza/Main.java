b3a11c2799b5669fe298d751560198473e654670
sailybaev <sailybaevvv@gmail,com>
2024-10-30 14:31:34 +0500

    removed Menu
    Add:
    New Pizzaz in Builder

--- src/main/java/duduPizza/Main.java
+++ src/main/java/duduPizza/Main.java
@@ -9,17 +9,14 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         PizzaFactory pizzaFactory = new PizzaFactory();
-        MenuService menuService = new MenuService();
         OrderService orderService = new OrderService();
 
-        // Create a Margherita Pizza with extra cheese
         Pizza margherita = pizzaFactory.createPizza("margherita");
         Pizza margheritaWithCheese = new CheeseTopping(margherita);
 
         System.out.println("Order: " + margheritaWithCheese.getDescription());
         System.out.println("Cost: $" + margheritaWithCheese.getCost());
 
-        // Place an order
         Order order = new Order(1, "Someone", Arrays.asList(margheritaWithCheese));
         Command placeOrder = new PlaceOrderCommand(orderService, order);
         placeOrder.execute();
